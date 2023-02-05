24d8386d434a0497337a63b8863094328a04883e
Farhan Jiratullah <farhanjiratullah794@gmail.com>
2023-02-05 09:37:30 +0700

    Add structure foldering part 2 and Form Requests

---
+++ app/Http/Requests/Doctor/StoreDoctorRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Doctor;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreDoctorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'specialist_id' => 'required|exists:specialists,id',
+            'fee' => 'required|string|max:255',
+            'photo' => 'required|string|max:255',
+        ];
+    }
+}
